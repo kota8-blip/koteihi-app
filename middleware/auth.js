cf060c689df4c6d17003683122393a83a5837429
kota8-blip <hattori0739@gmail.com>
2026-05-24 21:13:12 +0900

    プレミアムプラン申し込み時の戻るボタン押下時の遷移先修正②

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,7 @@
 export default function ({ store, redirect }) {
+  if (process.client && !store.state.jwt) {
+    store.commit('LOAD_FROM_STORAGE');
+  }
   if (!store.state.jwt) {
     return redirect('/login')
   }
